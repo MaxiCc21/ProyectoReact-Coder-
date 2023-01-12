2c5c717a97f995252a89dcb94612210a78553851
Maxi <maxi21498@gmail.com>
2023-01-12 13:15:11 -0300

    Galeria sin css(MethodGet)

---
+++ src/components/galeria/GaleriaApp.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import "./GaleriaApp.css";
+
+export default function GaleriaApp() {
+  const [db, setDb] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=6")
+      .then((res) => res.json())
+      .then((json) => setDb(json));
+  }, []);
+
+  return (
+    <div className="Galery-Container">
+      {db.map((el) => (
+        <Card key={el.id} el={el} />
+      ))}
+    </div>
+  );
+}
