3ba3e6893b87f7abf7e8dae1ce52cbee4b2d39f5
Maxi <maxi21498@gmail.com>
2023-01-24 16:49:27 -0300

    Sweet modal agregado

--- src/Hooks/useCard.jsx
+++ src/Hooks/useCard.jsx
@@ -1,18 +1,47 @@
 import { useState } from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 export const useCard = (props) => {
   const [itemListCard, setItemListCard] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+  const sweetalert2 = async () => {
+    const MySwal = withReactContent(Swal);
+    const res = await MySwal.fire({
+      title: "Seguro que desea eliminar este producto?",
+      showDenyButton: true,
+      denyButtonText: "Eliminar",
+    });
+
+    return res.value;
+  };
 
   const setItemToCard = (item) => {
     setItemListCard([...itemListCard, item]);
     setTotalPrice(totalPrice + item.price);
   };
 
-  const deleteItemToCard = (item) => {
+  const deleteItemToCard = async (item) => {
     console.log("Borrar");
-    let isDelete = window.confirm(`¿Seguro que desea eliminar ${item.id}`);
+    let isDelete = await sweetalert2();
+    console.log("respuesta", isDelete);
     if (isDelete) {
+      Toast.fire({
+        icon: "success",
+        title: "Producto elminiado",
+      });
       let resDelete = itemListCard.filter((el) => el.id !== item.id);
       setItemListCard(resDelete);
       setTotalPrice(totalPrice - item.price);
