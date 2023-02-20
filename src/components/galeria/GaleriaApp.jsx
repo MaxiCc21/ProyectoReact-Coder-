fc65c29a88949b873e5782edaaa7680104b064d9
MaxiCc21 <maxi21498@gmail.com>
2023-02-19 22:27:34 -0300

    hh

--- src/components/galeria/GaleriaApp.jsx
+++ src/components/galeria/GaleriaApp.jsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useFetch } from "../../Hooks/useFetch";
 import Card from "./Card";
 import "./GaleriaApp.css";
-
 import { useFireBase } from "../../Hooks/useFireBase";
 import Loader from "../loader/Loader";
 
 export default function GaleriaApp({ methodCardWidget, urlCategory,params }) {
-  // let { getAllData, db } = useFetch();
   let {listItem, getData} = useFireBase()
   const [loading, setLoading] = useState(false);
 
@@ -27,9 +24,7 @@ export default function GaleriaApp({ methodCardWidget, urlCategory,params }) {
     <div className="Galery-Container">
     {listItem && listItem.map((el) => (
       <Card key={el.id} el={el} methodCardWidget={methodCardWidget} />
-      
     ))}
   </div></>
-
   );
 }
